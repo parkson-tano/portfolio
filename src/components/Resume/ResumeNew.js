bfaceef070ae417c38f8e9e66aa2650be9a2261a
parkson-tano <tanoparksonsilencer@gmail.com>
2023-02-20 08:54:32 +0100

    fixed pdf

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -7,9 +7,9 @@ import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
+// https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf
 const resumeLink =
-  "https://www.dropbox.com/home/scan%20doc?preview=PARKSON+TANO+DANIEL.pdf";
+  "https://raw.githubusercontent.com/parkson-tano/portfolio/src/Assets/PARKSON-TANO-DANIEL.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -35,7 +35,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>

ec70350807010be6242c7d95f77bd9bb16c2c6f1
parkson-tano <tanoparksonsilencer@gmail.com>
2023-02-20 08:40:50 +0100

    update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,30 +8,44 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            I am <span className="purple"> Parkson Tano Daniel </span>
+            from <span className="purple"> Cameroon.</span>
+            <br />
+            I am a Python Fullstack Developer with experience in Django, React
+            JS, and Data Analytics. I have a passion for creating innovative and
+            engaging solutions for web applications and data analytics
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            My background in software engineering and data analytics has enabled
+            me to create projects that are both robust and efficient. I am a
+            problem solver who is constantly searching for new ways to utilize
+            my skills to find the best solutions
+            <br />
+            <br />I have a strong technical foundation and enjoy working with
+            data and developing software applications. I am eager to apply my
+            knowledge and skills to develop high-quality projects.
+
+            <br />
+            <br />
+            I also have strong working Knowledge of: 
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Algorithm
             </li>
             <li className="about-activity">
               <ImPointRight /> Writting Tech Blogs
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> <i class="fas fa-draw-square"></i>
+              DSA
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Parkson Tano</footer>
         </blockquote>
       </Card.Body>
     </Card>

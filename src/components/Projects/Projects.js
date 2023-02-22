388f8597105ce8d2e2e4ecea3cb9ea2a7f533d0a
parkson-tano <tanoparksonsilencer@gmail.com>
2023-02-22 21:51:22 +0100

    apid

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,7 +12,7 @@ import axios from 'axios'
 
 
 function Projects() {
-  const baseUrl = "https://portfolio-production-b853.up.railway.app/project";
+  const baseUrl = "https://portfolio-production-b853.up.railway.app/project/";
   const [project, setProject] = useState("")
   useEffect(() => {
     axios.get(baseUrl)
@@ -35,16 +35,22 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title={project}
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
+          {Object.entries(project)
+          .map(([key, pro]) => {
+            return (
+              <Col md={4} className="project-card">
+                <ProjectCard
+                  imgPath={pro.image}
+                  isBlog={pro.blog}
+                  title={pro.title}
+                  description= {pro.description}
+                  ghLink= {pro.github}
+                  demoLink= {pro.demo}
+                />
+              </Col>
+            );
+          })}
+          
         </Row>
       </Container>
     </Container>
